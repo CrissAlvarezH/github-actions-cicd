bd2faae8411a2181f52112bbd29d2969dcdabbe5
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-01-27 00:40:25 -0500

    testing pr 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div style={{display: "flex", justifyContent: "center", alignItems: "center", padding: "20px 20px"}}>
-        <h1>Testing PR 1</h1>
+        <h1>Testing PR 2</h1>
       </div>
   )
 }
