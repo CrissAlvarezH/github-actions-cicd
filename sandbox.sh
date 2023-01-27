8225c57866a92587646c3827642d596b51201aa7
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-01-26 22:53:55 -0500

    Add sandbox to gitignore

--- sandbox.sh
+++
@@ -1,5 +0,0 @@
-
-
-# commit=23f91d6
-docker build -t 23f91d6-commit .
-docker run --rm -d --label "traefik.http.routers.23f91d6.rule=Host(\`23f91d6.cristianalvarezh.com\`)" --network github-action-cicd_default --name 23f91d6-container 23f91d6-commit
