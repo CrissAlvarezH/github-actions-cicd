107d03e1647d3579de2791ede8eda77f42156c48
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-01-26 23:52:54 -0500

    Commit for test pipeline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div style={{display: "flex", justifyContent: "center", alignItems: "center", padding: "20px 20px"}}>
-        <h1>Hello Nuevo commit</h1>
+        <h1>Hello Nuevo commit aja</h1>
       </div>
   )
 }
